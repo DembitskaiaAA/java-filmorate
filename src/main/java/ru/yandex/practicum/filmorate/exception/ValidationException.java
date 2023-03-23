35e944c6ea46bf9a52776669b07be364dc82df6a
Nastya <tsutskova.an1996@yandex.ru>
2023-03-23 21:11:18 +0300

    Добавить исключения и их обработку

--- src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
@@ -4,8 +4,4 @@ public class ValidationException extends Throwable {
     public ValidationException(String msg) {
         super(msg);
     }
-
-    public String getInfo() {
-        return getMessage();
-    }
 }
